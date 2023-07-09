e0f72e85f48e577df6eeab29b4a9d0dfb7bc398a
Hawonee <5p3_10@naver.com>
2023-07-09 23:13:25 +0900

    230709

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,23 @@
 $(function(){ //시작
 
-  // header teams 호버 메뉴
-  // $('.gnb-item, .sub-list').hover(function(){
-    
-  //   $(this).children('.sub-list').toggleClass('active');
-  // });
-  $('.gnb-item').hover(function(){
-    if($(this).children('.sub-list').length){
-      $('.header').toggleClass('on');
-    }
-    $(this).children('.sub-list').toggleClass('active');
-  });
-
-
-
-  // 사이드 메뉴
-  
-  $('.btn-menu').click(function() {
-    $(this).toggleClass('on'); 
-    $(this).siblings('.side-nav').toggleClass('on');
-  });
-
-
-  // footer 관련 사이트
-  $('.btn-site').click(function(){
-    $('.site-list').toggleClass('active');
-});
-
-
-  ////////////// 슬라이드 
-
   var ppSlide = new Swiper(".sc-visual .swiper", {
     simulateTouch:false,
-    speed : 700,
-    loop: true,
+    loop:true,
     autoplay: {
-      delay: 3500,
+      delay: 2500,
       disableOnInteraction: false,
     },
   })
-  // swiper tip: 오토플레이 들어가면 자동 루프
+
+
+  $('select').change(function(){
+    $(this).css('color','#000')
+  })
+
+
   var ppSlide = new Swiper(".sc-people .swiper", {
     slidesPerView: 1,
+    // centeredSlides: true,
     spaceBetween: 30,
     navigation: {
       nextEl: ".next",
@@ -49,7 +25,7 @@ $(function(){ //시작
     },
     breakpoints:{
       768:{
-        slidesPerView:1,
+        slidesPerView: 1.2,
       },
     }
   })
@@ -63,11 +39,27 @@ $(function(){ //시작
     },
     breakpoints:{
       768:{
-        slidesPerView:2.3,
+        slidesPerView: 2,
       },
+      1025:{
+        slidesPerView: 3,
+      }
     }
   })
 
 
+
+  // footer 관련 사이트
+  $('.btn-site').click(function(){
+    $('.relate-site').toggleClass('active');
+});
+
+// header teams 호버 메뉴
+  $('.gnb-item').hover(function(){
+    if($(this).children('.sub-list').length){
+      $('.header').toggleClass('on');
+    }
+    $(this).children('.sub-list').toggleClass('active');
+  });
 
 }); //지우지 마시오
